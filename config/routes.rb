efab87d7ca8ed748fa7fb828938fb4d2fb9d1700
OpheNg <ophe.ng@live.fr>
2023-03-08 21:56:27 +0100

    model contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :contacts, only: [:new, :create]
 
   get 'myspace', to: 'pages#myspace'
-  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'profile', to: 'pages#profile'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
